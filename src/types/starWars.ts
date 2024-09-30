75aef47220634d2f178ba5b40b90b3fd511d0199
Augusto L <juanaugustolombino@protonmail.ch>
2024-09-30 14:26:42 -0300

    Move the interfaces to their own file/folder

---
+++ src/types/starWars.ts
@@ -0,0 +1,30 @@
+/*Common interface for all starwars vehicles*/
+interface Spacecraft {
+  name: string;
+  model: string;
+  manufacturer: string;
+  length: string;
+  cost_in_credits: string;
+  crew: string;
+  passengers: string;
+  max_atmosphering_speed: string;
+  cargo_capacity: string;
+  consumables: string;
+  films: string[];
+  pilots: string[];
+  url: string;
+  created: string;
+  edited: string;
+}
+
+interface Vehicle extends Spacecraft {
+  vehicle_class: string;
+}
+
+interface Starship extends Spacecraft {
+  starship_class: string;
+  hyperdrive_rating: string;
+  MGLT: string;
+}
+
+export type { Vehicle, Starship }
