a5be294b1aae312c0a19e66bc7b2dc6e0f359754
Augusto L <juanaugustolombino@protonmail.ch>
2024-10-02 22:53:52 -0300

    Update SacecraftDialog
    If the Dialog is open and the spacecraft has movies, fetch them from SWAPI, and show them. Also, with some of the Typography elements, only show them if they're not empty.

--- src/components/SpacecraftDialog.tsx
+++ src/components/SpacecraftDialog.tsx
@@ -1,8 +1,11 @@
+import React, { useEffect, useState } from "react";
 import Dialog from '@mui/material/Dialog';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Vehicle, Starship } from "../types/starWars";
+import { fetchFilmByUrl } from '../api/swapi';
+import { CircularProgress } from "@mui/material";
 
 
 interface SpacecraftDialogProps {
@@ -13,6 +16,32 @@ interface SpacecraftDialogProps {
 
 
 const SpacecraftDialog: React.FC<SpacecraftDialogProps> = ({ open, spacecraft, onClose }) => {
+
+  const [films, setFilms] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (open && spacecraft.films.length > 0) {
+      setLoading(true);
+      const fetchData = async (filmUrl: string) => {
+        if (filmUrl != "") {
+          try {
+            const fetchedFilm = await fetchFilmByUrl(filmUrl);  
+            setFilms(prevFilms => [...prevFilms, fetchedFilm.title]);
+          } catch (error) {
+            console.error('Error fetching films data:', error);
+          } finally {
+            setLoading(false);
+          }
+        }
+      }
+      spacecraft.films.forEach(filmUrl => fetchData(filmUrl));
+    }
+    else {
+      setFilms([]);
+    }
+  }, [open, spacecraft]);
+
   return (
     <Dialog open={open} onClose={onClose} className='spacecraft-dialog' sx={{ '& .MuiDialog-paper': { backgroundColor: 'transparent' } }}>
       <Card className='spacecraft-card'>
@@ -32,46 +61,69 @@ const SpacecraftDialog: React.FC<SpacecraftDialogProps> = ({ open, spacecraft, o
           <Typography variant="subtitle1" className="card-text" align="left">
             <b className="important">Cost: </b>{spacecraft.cost_in_credits === "" ? "unknown price" : `ᖬ${spacecraft.cost_in_credits}`}
           </Typography>
-          <Typography variant="subtitle1" className="card-text" align="left">
-            <b className="important">Length: </b>{spacecraft.length}
-          </Typography>
-          <Typography variant="subtitle1" className="card-text" align="left">
-            <b className="important">Crew: </b>{spacecraft.crew}
-          </Typography>
-          <Typography variant="subtitle1" className="card-text" align="left">
-            <b className="important">Passengers: </b>{spacecraft.passengers}
-          </Typography>
-          <Typography variant="subtitle1" className="card-text" align="left">
-            <b className="important">Max Atmosphering Speed: </b>{spacecraft.max_atmosphering_speed}
-          </Typography>
-          <Typography variant="subtitle1" className="card-text" align="left">
-            <b className="important">Cargo Capacity: </b>{spacecraft.cargo_capacity}
-          </Typography>
-          <Typography variant="subtitle1" className="card-text" align="left">
-            <b className="important">Consumables: </b>{spacecraft.consumables}
-          </Typography>
-          {spacecraft.type === 'vehicle' && 
+          {spacecraft.length &&
+            <Typography variant="subtitle1" className="card-text" align="left">
+              <b className="important">Length: </b>{spacecraft.length}
+            </Typography>
+          }
+          {spacecraft.crew &&
+            <Typography variant="subtitle1" className="card-text" align="left">
+              <b className="important">Crew: </b>{spacecraft.crew}
+            </Typography>
+          }
+          {spacecraft.passengers &&
+            <Typography variant="subtitle1" className="card-text" align="left">
+              <b className="important">Passengers: </b>{spacecraft.passengers}
+            </Typography>
+          }
+          {spacecraft.max_atmosphering_speed &&
+            <Typography variant="subtitle1" className="card-text" align="left">
+              <b className="important">Max Atmosphering Speed: </b>{spacecraft.max_atmosphering_speed}
+            </Typography>
+          }
+          {spacecraft.cargo_capacity &&
+            <Typography variant="subtitle1" className="card-text" align="left">
+              <b className="important">Cargo Capacity: </b>{spacecraft.cargo_capacity}
+            </Typography>
+          }
+          {spacecraft.consumables &&
+            <Typography variant="subtitle1" className="card-text" align="left">
+              <b className="important">Consumables: </b>{spacecraft.consumables}
+            </Typography>
+          }
+          {spacecraft.type === 'vehicle' && (spacecraft as Vehicle).vehicle_class &&
             <Typography variant="subtitle1" className="card-text" align="left">
               <b className="important">Vehicle Class: </b>{(spacecraft as Vehicle).vehicle_class}
             </Typography>
           }
           {spacecraft.type === 'starship' && 
             <>
-              <Typography variant="subtitle1" className="card-text" align="left">
-                <b className="important">Starship Class: </b>{(spacecraft as Starship).starship_class}
-              </Typography>
-              <Typography variant="subtitle1" className="card-text" align="left">
-                <b className="important">Hyperdrive Rating: </b>{(spacecraft as Starship).hyperdrive_rating}
-              </Typography>
-              <Typography variant="subtitle1" className="card-text" align="left">
-                <b className="important">Megalights per hour: </b>{(spacecraft as Starship).MGLT === "" ? "unknown" : (spacecraft as Starship).MGLT}
-              </Typography>
+              {(spacecraft as Starship).starship_class &&
+                <Typography variant="subtitle1" className="card-text" align="left">
+                  <b className="important">Starship Class: </b>{(spacecraft as Starship).starship_class}
+                </Typography>
+              }
+              {(spacecraft as Starship).hyperdrive_rating &&
+                <Typography variant="subtitle1" className="card-text" align="left">
+                  <b className="important">Hyperdrive Rating: </b>{(spacecraft as Starship).hyperdrive_rating}
+                </Typography>
+              }
+              {(spacecraft as Starship).MGLT &&
+                <Typography variant="subtitle1" className="card-text" align="left">
+                  <b className="important">Megalights per hour: </b>{(spacecraft as Starship).MGLT === "" ? "unknown" : (spacecraft as Starship).MGLT}
+                </Typography>
+              }
             </>
           }
-          {spacecraft.films.map(film => 
-            <Typography key={film} variant="subtitle1" className="card-text" align="left">
-              - {film}
-            </Typography>)}
+          {spacecraft.films.length > 0 &&
+            <Typography variant="subtitle1" className="card-text" align="left">
+              <b className="important">Films: </b>
+              { loading ? 
+                (<CircularProgress size={24}/>) : 
+                (films.map((film, index) => <>{film}{index === films.length - 1 ? "." : ", "}</>))
+              }
+            </Typography>
+          }
         </CardContent>
       </Card>
     </Dialog>
