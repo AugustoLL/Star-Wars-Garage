50f388115a2cf1ae017256abc349d32ddb91fdde
Augusto L <juanaugustolombino@protonmail.ch>
2024-10-02 23:37:34 -0300

    Prevent fetching the same URL multiple times

--- src/api/swapi.ts
+++ src/api/swapi.ts
@@ -5,18 +5,40 @@ import { API_URL } from '../constants';
 /**
  * Fetches all items from the given URL, by following the "next" pagination links.
  * The initial URL is passed as a parameter and then it is updated with the "next" url if it exists.
+ * 
+ * Tries to prevent fetching the same URL multiple times using a Set.
+ * (This needs improvement in the future)
  */
 const fetchAllItems = async <Spacecraft>(initialUrl: string): Promise<Spacecraft[]> => {
   let allItems: Spacecraft[] = [];
   let url = initialUrl;
+  const fetchedUrls = new Set<string>(); // To track already fetched URLs
 
   while (url) {
-    const response = await fetch(url);
-    const data = await response.json();
+    if (fetchedUrls.has(url)) {
+      console.warn(`Skipping duplicate URL: ${url}`);
+      break;
+    }
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data from: ${url}`);
+      }
+      const data = await response.json();
 
-    allItems = [...allItems, ...data.results];
+      allItems = [...allItems, ...data.results];
+      fetchedUrls.add(url);
+      url = data.next;
+    } catch(error) {
+      console.error(`Error fetching items from ${url}:`, error);
+      break;
+    }
+    // const response = await fetch(url);
+    // const data = await response.json();
 
-    url = data.next;
+    // allItems = [...allItems, ...data.results];
+
+    // url = data.next;
   }
   
   return allItems;
